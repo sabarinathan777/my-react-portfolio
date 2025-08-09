26dd611b3e5de20b2bb625dadec5c248603b83f1
sabarinathan7 <134140871+sabarinathan7@users.noreply.github.com>
2025-08-09 23:58:42 +0530

    Cv Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
         </p>
 
         <motion.a
-          href="DOC/SabariNathan_FrontEnd.pdf"
+          href="DOC/cv.pdf"
           download="Sabari_Nathan_FrontEnd.pdf"
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
