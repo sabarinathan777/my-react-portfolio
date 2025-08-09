bd4951f879b1290af6d1817b0337e88a61195059
sabarinathan7 <134140871+sabarinathan7@users.noreply.github.com>
2025-08-10 00:04:08 +0530

    Cv Location Change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
         </p>
 
         <motion.a
-          href="DOC/cv.pdf"
+          href="DOC/cv.pdf.pdf"
           download="Sabari_Nathan_FrontEnd.pdf"
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
